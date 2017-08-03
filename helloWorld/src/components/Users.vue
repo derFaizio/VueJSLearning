0bb5a272eb7b7eb8c4ad2d7a9a3e48f6600a8d66
derFaizio <aslam.mfaizan@gmail.com>
2017-08-03 15:41:51 +0200

    Updated Users.Vue to Add, delete users.

--- helloWorld/src/components/Users.vue
+++ helloWorld/src/components/Users.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
         <h1>This is the Users page</h1>
-        <p>User Name is {{ $route.params.userName }}</p>
+        <!--p>User Name is {{ $route.params.userName }}</p-->
+        <h2>List of Users</h2>
+        <input type="text" v-model="newUser" @keyup.enter='addUser' placeholder="Add a new user">
+        <button class="btn btn-info" @click='addUser'>Add</button>
         <div>
-            <ul>
-                <li v-for="user in users" class="active"><a href="#">{{ user.userName }}</a></li>
-            </ul>
+            <h3 v-for="user in users" class="active"><a href="#">{{ user.userName }}</a>
+                <button class="destroy" @click="deleteUser(user)">X</button>
+            </h3>
         </div>
         <button @click='goHome'>Go Home</button>
     </div>
@@ -19,11 +22,19 @@ export default {
     }, 
     {
         userName: "Bob"
-    }]
+    }], 
+    newUser: ''
   }),
     methods:{
       goHome(){
         this.$router.push('/home');
+      },
+      addUser(){
+        this.users.push({userName: this.newUser});
+        this.newUser = ""
+      },
+      deleteUser(user){
+          this.users.splice(this.users.indexOf(user), 1)
       }
   }
 
