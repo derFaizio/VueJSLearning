65747ec6474316e93d8c885164910754802c7318
derFaizio <aslam.mfaizan@gmail.com>
2017-08-04 10:27:02 +0200

    Added a user component to display the user details.

--- helloWorld/src/components/Users.vue
+++ helloWorld/src/components/Users.vue
@@ -6,9 +6,10 @@
         <input type="text" v-model="newUser" @keyup.enter='addUser' placeholder="Add a new user">
         <button class="btn btn-info" @click='addUser'>Add</button>
         <div>
-            <h3 v-for="user in users" class="active"><a href="#">{{ user.name }}</a>
+            <h3 v-for="user in users" :key="user.key" class="active"><a href="#">{{ user.name }}</a>
                 <button class="destroy" @click="deleteUser(user)">X</button>
             </h3>
+            <user v-for="user in users" :key="user.key" :userName= "user.name"></user>
         </div>
         <button @click='goHome'>Go Home</button>
     </div>
@@ -16,36 +17,40 @@
 
 <script>
 import {firebaseDB} from '../firebase';
+import User from './User.vue'
 
 export default {
-  data: () => ({
-    users: {}, 
-    newUser: ''
-  }),
-  firebase: {
-    users: {
-      source: firebaseDB.ref('users'),
-      // Optional, allows you to handle any errors.
-      cancelCallback(err) {
-        console.error(err);
-      }
-    }
-  },
-    methods:{
-      goHome(){
-        this.$router.push('/home');
-      },
-      addUser(){
-        var value = this.newUser && this.newUser.trim()
-        if (!value) {
-            return
+    data: () => ({
+        users: {}, 
+        newUser: ''
+    }),
+    firebase: {
+        users: {
+        source: firebaseDB.ref('users'),
+        // Optional, allows you to handle any errors.
+        cancelCallback(err) {
+            console.error(err);
         }
-        this.$firebaseRefs.users.push({name: this.newUser})
-        this.newUser = ""
-      },
-      deleteUser(user){
-          this.$firebaseRefs.users.child(user['.key']).remove()
-      }
+        }
+    },
+    methods:{
+        goHome(){
+            this.$router.push('/home');
+        },
+        addUser(){
+            var value = this.newUser && this.newUser.trim()
+            if (!value) {
+                return
+            }
+            this.$firebaseRefs.users.push({name: this.newUser})
+            this.newUser = ""
+        },
+        deleteUser(user){
+            this.$firebaseRefs.users.child(user['.key']).remove()
+        }
+    }, 
+    components: {
+        'user': User
   }
 
 }
